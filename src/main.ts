054a1431ed91822f82e592c9745bff75ddb86d63
mehediemon007 <mehediemon7@gmail.com>
2026-01-11 20:18:55 +0600

    fix-deploy

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,25 @@ import { ExpressAdapter } from '@nestjs/platform-express';
 import express, { Request, Response } from 'express';
 
 const server = express();
+let cachedApp: any; // Variable to store the initialized app
 
 export const bootstrap = async () => {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  // If app is already initialized, don't do it again
+  if (!cachedApp) {
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
-  app.enableCors();
-  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
-  app.setGlobalPrefix('api');
+    app.enableCors();
+    app.useGlobalPipes(
+      new ValidationPipe({ transform: true, whitelist: true }),
+    );
+    app.setGlobalPrefix('api');
 
-  await app.init();
+    await app.init();
+    cachedApp = app; // Store it for the next request
+  }
   return server;
 };
 
-// Use explicit types for Request and Response to satisfy TypeScript
 export default async (req: Request, res: Response) => {
   await bootstrap();
   server(req, res);
