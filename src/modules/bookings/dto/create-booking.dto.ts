778adacb2757fb1fc5716635c5acc97a4a8228a4
mehediemon007 <mehediemon7@gmail.com>
2026-02-05 21:43:44 +0600

    update-booking-dto

--- src/modules/bookings/dto/create-booking.dto.ts
+++ src/modules/bookings/dto/create-booking.dto.ts
@@ -1,6 +1,5 @@
 import {
   IsNotEmpty,
-  IsOptional,
   IsUUID,
   IsString,
   IsDateString,
@@ -9,26 +8,30 @@ import {
 
 export class CreateBookingDto {
   @IsUUID()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Chamber ID is required' })
   chamberId: string;
+
+  @IsUUID()
+  @IsNotEmpty({ message: 'Please select a specific shift' })
+  shiftId: string;
 
   /**
    * Must be in YYYY-MM-DD format.
    * Example: "2026-01-15"
    */
   @IsDateString()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Booking date is required' })
   bookingDate: string;
 
-  @IsOptional()
   @IsString()
-  fullName?: string;
+  @IsNotEmpty({ message: 'Patient  name is required' })
+  fullName: string;
 
-  @IsOptional()
+  @IsNotEmpty({ message: 'Phone number is required' })
   @Matches(/^\+8801[3-9]\d{8}$/, {
     message:
       'Phone number must be a valid Bangladesh number starting with +880 (e.g., +88017XXXXXXXX)',
   })
   @IsString()
-  phone?: string;
+  phone: string;
 }
